d03f1a78afc1f4a65e11893b35be5f4165320f5c
florianharmand <florian.harmand@gmail.com>
2023-04-11 16:24:35 +0200

    delete install sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-mkdir public
-mkdir assets
-
-curl -o "./assets/citeproc-style.csl" https://www.zotero.org/styles/iso690-author-date-fr-no-abstract
-curl -o "./assets/citeproc-i18n.xml" https://raw.githubusercontent.com/citation-style-language/locales/6b0cb4689127a69852f48608b6d1a879900f418b/locales-fr-FR.xml
-
-python3 bibliography.py
